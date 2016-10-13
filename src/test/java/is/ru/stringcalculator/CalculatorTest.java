830d462c4a1655a5446f8f730c1a5163d8876cb7
hrafnhildursig15 <hrafnhildursig15@ru.is>
2016-10-13 17:50:20 +0000

    Folders bin and src added

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -5,10 +5,6 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
-	public static void main(String args[]) {
-      org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
-    }
-
 	@Test
 	public void testEmptyString() {
 		assertEquals(0, Calculator.add(""));
@@ -18,15 +14,5 @@ public class CalculatorTest {
 	public void testOneNumber() {
 		assertEquals(1, Calculator.add("1"));
 	}
-
-	@Test
-	public void testTwoNumbers() {
-		assertEquals(3, Calculator.add("1,2"));
-	}	
-
-	@Test
-    public void testMultipleNumbers(){
-    	assertEquals(6, Calculator.add("1,2,3"));
-    }
 
 }
