00044eb055aca5b5d1a9ebeff020fd49f4d2affe
hrafnhildursig15 <hrafnhildursig15@ru.is>
2016-10-13 20:35:56 +0000

    Added function to replace \n with a comma

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,9 +3,30 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text){
-		if(text.equals(""))
+		if(text.equals("")){
 			return 0;
+		}
+		else if(text.contains(",")){
+			return sum(splitNumbers(replaceNewLine(text)));
+		}
 		else
 			return 1;
+	}
+	private static String replaceNewLine(String text){
+		return text.replace("\n", ",");
+	}
+
+	private static int toInt(String number){
+		return Integer.parseInt(number);
+	}
+	private static String[] splitNumbers(String numbers){
+		return numbers.split(",");
+	}
+	private static int sum(String[] numbers){
+		int total = 0;
+		for(String number : numbers){
+			total += toInt(number);
+		}			
+		return total;
 	}
 }
